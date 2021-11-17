409d689e5242870a2b4d22f89aeac816d619c1db
Taylor Falk <tfalk@scc-tk2.scc.bu.edu>
2021-11-17 11:32:00 -0500

    Created Rmarkdown and associated files

--- main.R
+++ main.R
@@ -4,3 +4,34 @@
 ## BU BF591
 ## Assignment Week 2
 
+#### Bioconductor ####
+load_bioconductor <- function() {
+  # have students find correct install lines from https://bioconductor.org/install/ ?
+  # note that this is different for the current version of Bioconductor than the 528 tutorial
+  if (!require("BiocManager", quietly = TRUE)){
+    install.packages("BiocManager")
+  }
+  BiocManager::install(c('affy','affyPLM','sva',
+                         'AnnotationDbi','hgu133plus2.db'), 
+                       version = "3.13")
+  library(affy)
+  library(affyPLM)
+  library(sva)
+  library(AnnotationDbi)
+  library(hgu133plus2.db)
+}
+
+load_expression <- function(filepath) {
+  # Load the csv of expression data from the filepath variable, returns stored object
+  expression <- read.csv(filepath, header = TRUE, sep = " ")
+  return(expression)
+}
+
+#### Markdown ####
+source(knitr::purl('report.Rmd'))
+print(print1())
+
+if(interactive()) {
+  # load_bioconductor()
+  # expr <- load_expression('/project/bf528/project_1/data/example_intensity_data.csv')
+}
